19907116f477246df6d7cf0f477dfa90d54cb609
Chalapathi123 <70141017+Chalapathi123@users.noreply.github.com>
2021-12-13 10:15:17 +0530

    Update Positions.java

--- Positions.java
+++ Positions.java
@@ -19,29 +19,11 @@ public class Positions {
     public void SetLadder(int start,int end){
         this.ladders.put(start,end); 
     }
-    // public int getPlayerPosition(String name){
-    //     return this.players.get(name); 
-    // }
+
     public boolean isSnakePosition(int posi){
         return (this.snakes.containsKey(posi)); 
     }
     public boolean isLadderPosition(int posi){
         return (this.ladders.containsKey(posi)); 
     }
-
-    // public void SetPlayer(String Name,int posi){
-    //     this.players.put(Name,posi); 
-    // }
-    // public void getPlayers(){
-    //     for (Map.Entry<String,Integer> entry : players.entrySet())
-    //         System.out.println("Name= " + entry.getKey() + ",Position= " + entry.getValue());
-    // } 
-    // public void getSnakes(){
-    //     for (Map.Entry<Integer,Integer> entry : snakes.entrySet())
-    //         System.out.println("Head= " + entry.getKey() + ",tail = " + entry.getValue());
-    // }
-    // public void getLadder(){
-    //     for (Map.Entry<Integer,Integer> entry : ladders.entrySet())
-    //         System.out.println("Head= " + entry.getKey() + ",tail = " + entry.getValue());
-    // }  
 }
